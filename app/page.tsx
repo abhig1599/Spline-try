f123f456b72e3af1d59d430c37e749e4b010bed1
Abhishek Guleria <120269927+abhig1599@users.noreply.github.com>
2024-10-22 23:16:07 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,13 @@
-import Image from "next/image";
+import React from 'react';
 import Spline from '@splinetool/react-spline/next';
+import "@/app/globals.css";
 
-export default function Home() {
+const Page = () => {
   return (
-    <div className="relative h-screen w-screen overflow-hidden">  
-      
-      {/* Spline background */}
-      <div className="absolute inset-0 z-0 mt-8">
-        <Spline 
-          scene="https://prod.spline.design/oApcLuPrMhxCaOGa/scene.splinecode" 
-          className="w-full h-full"
-        />
-      </div>
-      
-      {/* Content */}
-      <div className="relative z-10 flex items-center justify-center h-full">
-        <div className="text-white text-center mt-[-600px]">
-          <h1 className="text-4xl font-bold">Welcome to the Landing Page</h1>
-          <p className="mt-4">Your content goes here</p>
-        </div>
-      </div>
+    <div>
+      <Spline scene="https://prod.spline.design/NFA0orc-7uu82gHS/scene.splinecode" />
     </div>
   );
-}
+};
+
+export default Page;
